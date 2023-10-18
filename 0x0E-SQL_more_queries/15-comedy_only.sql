2bae65f3c8e8b7f706b6379d0d5b0617ef11efac
MahmoudAdelOsman <mahmoud.adel.elgandy@gmail.com>
2023-10-18 11:08:48 -0700

    up3

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,5 @@
---lists all Comedy shows in the database
-SELECT tv_shows.title AS title
-FROM tv_shows
-JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY title;
